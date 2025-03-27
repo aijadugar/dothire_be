acbb1ec3d0eeafeebc9ed5bde4ff211e0daee99b
OnlyCR7 <139578960+aijadugar@users.noreply.github.com>
2025-03-27 18:34:43 +0530

    Delete resumes directory

--- resumes/bias_detection.py
+++
@@ -1,8 +0,0 @@
-from aif360.algorithms.preprocessing import Reweighing
-from aif360.datasets import BinaryLabelDataset
-
-def detect_bias(resumes):
-    dataset = BinaryLabelDataset(df=resumes, label_names=['job_match_score'], protected_attribute_names=['gender'])
-    rw = Reweighing()
-    debiased_dataset = rw.fit_transform(dataset)
-    return debiased_dataset.convert_to_dataframe()[0]
